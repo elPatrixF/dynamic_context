d1088b250835cc4f19f30511a98a7c49fcd867cb
elPatrixF <30423060+elPatrixF@users.noreply.github.com>
2023-05-28 23:32:52 -0300

    Update script.py
    Added date to prompt context, and added UI elements.

--- script.py
+++ script.py
@@ -2,6 +2,10 @@ import os
 import gradio as gr
 from datetime import datetime
 
+params = {
+	'timecontext': True,
+	'datecontext': False,
+}
 
 def	custom_generate_chat_prompt(user_input, state, **kwargs):
 	'''
@@ -20,8 +24,16 @@ def	custom_generate_chat_prompt(user_input, state, **kwargs):
 
 def input_modifier(string):
 	"""
-	This function adds time context to each prompt.
+	This function adds time or date context to each prompt.
 	"""
-	string += f' [Current time: {datetime.now().strftime("%I:%M %p")}]'
+	if params['timecontext'] : string += f' [Current time: {datetime.now().strftime("%I:%M %p")}]'
+	if params['datecontext'] : string += f' [Current date: {datetime.now().strftime("%B %d, %Y")}]'
 	return string
 
+
+def ui():
+	with gr.Accordion("Prompt context"):
+		timecontext = gr.Checkbox(value=params['timecontext'], label='Add current time context to the end of the prompt')
+		datecontext = gr.Checkbox(value=params['datecontext'], label='Add current date context to the end of the prompt')
+	timecontext.change(lambda x: params.update({"timecontext": x}), timecontext, None)
+	datecontext.change(lambda x: params.update({"datecontext": x}), datecontext, None)
